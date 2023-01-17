33b85c8addbba64e1dfafecbbd38cd67010f649d
Evgeny-You <eyo5825@gmail.com>
2023-01-17 15:59:48 +0300

    Поработал с функциями.

---
+++ lesson_3/Types/dict_.py
@@ -0,0 +1,14 @@
+# a = {'hallo'}
+# dict_ = dict(a)
+# print(a)
+
+d = {}
+print(d)
+d = {'dict': 1, 'dictionary': 2}
+print(d)
+
+d = dict(short='dict', long='dictionary')
+print(d)
+
+d = dict([(1, 1), (2, 4)])
+print(d)
---
+++ lesson_3/Types/list_.py
@@ -0,0 +1,34 @@
+#list_ = []
+#print(list_)
+
+#list_ = [1, 4, 6]
+#list_.append(2)
+#print(list_)
+
+#list_ = [1, 4, 6]
+#list_.insert(0,7)
+#list_.insert(2,10)
+#print(list_)
+#print(list_[3])
+#list_.pop(3)
+#print(list_)
+#list_.sort()
+#print(list_)
+#list_.sort(reverse=True)
+#print(list_)
+#list_.sort(reverse=False)
+#print(list_)
+
+#list_ = [1, 3, 4, 6]
+#list_.remove(1)
+#print(list_)
+
+# list_ = [1, 3, 4, 6]
+# list_.clear()
+# print(list_)
+
+# list_ = [1, 3, 4, 6]
+# list_1 = list_.copy()
+# print(list_1)
+
+
