1f5e2acb160db50448235d012185ae4e36f845d3
Evgeny-You <eyo5825@gmail.com>
2023-02-09 06:29:42 +0300

    Факториал добавлена функция

--- lesson_6/task_2.py
+++ lesson_6/task_2.py
@@ -1,6 +1,15 @@
-n = int(input('Введите число: '))
-factorial = 1
-while n > 1:
-    factorial *= n
-    n -= 1
-print(factorial)
+# n = int(input('Введите число: '))
+# factorial = 1
+# while n > 1:
+#     factorial *= n
+#     n -= 1
+# print(factorial)
+
+def factorial(n):
+    proizvedenie = 1
+    for i in range(2, n+1):
+        proizvedenie = proizvedenie * i
+        print(proizvedenie)
+
+
+factorial(5)
